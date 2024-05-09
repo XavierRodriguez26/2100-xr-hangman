9ccf56b27008697efc0b7decbb676b296cc308a2
XavierRodriguez26 <91894689+XavierRodriguez26@users.noreply.github.com>
2024-05-08 22:47:27 -0400

    FINAL COMMENTS AND NOTES BEFORE PRESENTATION

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Initializing all the variables we created in the HTML 
 
-const WordContainer = document.getElementById('letter-container');
+// This entire seciton of code is assigning constant variables to the variables that are withing our index.html page to use through our code
+const WordContainer = document.getElementById('letter-container'); 
 const OptionsContainer = document.getElementById('options-container');
 const UserInput = document.getElementById('user-input-section');
 const NewGame = document.getElementById('new-game-container');
@@ -42,27 +43,30 @@ let options = {
 
 // WIN COUNTER
 
-let winCounter = 0;
+let winCounter = 0; 
 let count = 0;
 
-let chosenWord = ""; // This is the code for the random word that will be generated each game
+let chosenWord = ""; // Assigning an empty string that will be replaced with the value from any of the words from the chosenWord array elements 
 
-//Display option buttons
-const displayOptions = () => {
-  OptionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
-  let buttonCon = document.createElement("div");
-  for (let value in options) {
+//Display option buttons 
+
+// This section of code is creating the buttons at the top of the screen for each Category of words you can choose to pick from in this game. 
+const displayOptions = () => { 
+  OptionsContainer.innerHTML += `<h3>Please Select An Option</h3>`; // Adding the H3 element into the OptionsContainer with innerHTML to display the heading on our screen. 
+  let buttonCon = document.createElement("div"); 
+  for (let value in options) {   
     buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
   }
-  OptionsContainer.appendChild(buttonCon);
+  OptionsContainer.appendChild(buttonCon); // We are now appending the buttonCon which are the botton variables for each category (CAr brand, Animals, Colors) and linking it to the OptionsContainer which will display the letters and hashes for each word. 
 };
 
 //Block all the Buttons
-const blocker = () => {
-  let optionsButtons = document.querySelectorAll(".options");
-  let letterButtons = document.querySelectorAll(".letters");
+const blocker = () => {  // Assigning constant variable as "blocker"
+  let optionsButtons = document.querySelectorAll(".options");  // Creating sub variables under the blocker for the "options"... once we click a button for the category of words, they will not be able to be found again until the next game
+  let letterButtons = document.querySelectorAll(".letters"); // Blockling all of the letters that we have guessed already from being selected twice
+
   //disable all options
-  optionsButtons.forEach((button) => {
+  optionsButtons.forEach((button) => { // Each time  
     button.disabled = true;
   });
 
@@ -170,21 +174,28 @@ const initializer = () => {
 
 // CANVAS 
 const canvasCreator = () => {
-    let context = canvas.getContext("2d");
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = 2;
+    let context = canvas.getContext("2d"); // Get the 2D drawing context of the canvas
+    context.beginPath(); // Start a new path
+    context.strokeStyle = "black"; // Set stroke color to black
+    context.lineWidth = 2; // Set the line width
 
 
 // Line Drawing paramaters
 const drawLine = (fromX, fromY, toX, toY) => {
-    context.moveTo(fromX, fromY);
-    context.lineTo(toX, toY);
-    context.stroke();
+    context.moveTo(fromX, fromY); // Move the "pen" to the starting point
+    context.lineTo(toX, toY); // Draw a line to the ending point
+    context.stroke(); // Stroke the path
 };
 
 // BODY PART DRAWINGS
     
+  // fromX: The x-coordinate of the starting point of the line.
+  // fromY: The y-coordinate of the starting point of the line.
+  // toX: The x-coordinate of the ending point of the line.
+  // toY: The y-coordinate of the ending point of the line.
+
+
+
     // HEAD
     const head = () => {
         context.beginPath();
@@ -194,17 +205,17 @@ const drawLine = (fromX, fromY, toX, toY) => {
 
     //BODY
     const body = () => {
-      drawLine(70, 40, 70, 80);
+      drawLine(70, 40, 70, 80); // draws a vertical line from the point (70, 40) to the point (70, 80), which represents the body of the hangman.
     };
 
     //LEFT ARM
     const leftArm = () => {
-        drawLine(70, 50, 50, 70); 
+        drawLine(70, 50, 50, 70);  // draws a line from the point (70, 50) to the point (50, 70), which represents the left arm of the hangman.
     };
 
     // RIGHT ARM
     const rightArm = () => {
-        drawLine(70, 50, 90, 70); 
+        drawLine(70, 50, 90, 70); //  draws a line from the point (70, 50) to the point (90, 70), which represents the right arm of the hangman.
     };
 
     //LEFT LEG
@@ -223,7 +234,7 @@ const drawLine = (fromX, fromY, toX, toY) => {
     //clear canvas
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     //bottom line
-    drawLine(10, 130, 130, 130);
+    drawLine(10, 130, 130, 130); 
     //left line
     drawLine(10, 10, 10, 131);
     //top line
@@ -236,10 +247,19 @@ const drawLine = (fromX, fromY, toX, toY) => {
 };
 
 //draw the man
+
+// Destructure the functions head, body, leftArm, rightArm, leftLeg, and rightLeg from the canvasCreator
+// These functions are responsible for drawing different body parts of the hangman on the canvas
+
+// EACH CASE WORKS IN THE FOLLOWING ORDER
+
+// CASE 1 (count 1) = call the head function to draw the head of the hangman
+// CASE 2 (count 2) = call the body function and SO ON FOR THE FOLLOWING CASES UNTIL THE COUNT MAX IS HIT
 const drawMan = (count) => {
+  // Use a switch statement to determine which body part to draw based on the count
   let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
   switch (count) {
-    case 1:
+    case 1:     
       head();
       break;
     case 2:
